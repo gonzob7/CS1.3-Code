c56820dac369659788afd34dad21460997fa7d95
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-04-21 21:27:07 -0700

    finished is_palindrome_iterative method

---
+++ palindromes.py
@@ -0,0 +1,72 @@
+#!python
+
+import string
+# Hint: Use these string constants to ignore capitalization and/or punctuation
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+
+
+def is_palindrome(text):
+    """A string of characters is a palindrome if it reads the same forwards and
+    backwards, ignoring punctuation, whitespace, and letter casing."""
+    # implement is_palindrome_iterative and is_palindrome_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    assert isinstance(text, str), 'input is not a string: {}'.format(text)
+    return is_palindrome_iterative(text)
+    # return is_palindrome_recursive(text)
+
+
+def is_palindrome_iterative(text):
+    # TODO: implement the is_palindrome function iteratively here
+
+    right_char = len(text)-1 #right pointer
+    left_char = 0 #left pointer
+
+
+    if len(text) <= 1: #when length of string is 1 or 0, it will always be a palindrome
+        return True
+
+    while text[left_char].lower() == text[right_char].lower(): #loops as long as characters match
+        if left_char + 1 == right_char: #determines if we are in the last two or one character
+            return True
+        elif left_char + 2 == right_char:
+            return True
+
+        right_char -= 1
+        left_char += 1
+
+    return False #loop exit, means it is not a palindrome
+
+
+
+    # once implemented, change is_palindrome to call is_palindrome_iterative
+    # to verify that your iterative implementation passes all tests
+
+
+def is_palindrome_recursive(text, left=None, right=None):
+    # TODO: implement the is_palindrome function recursively here
+    pass
+    # once implemented, change is_palindrome to call is_palindrome_recursive
+    # to verify that your iterative implementation passes all tests
+
+
+def main():
+    import sys
+
+    print(is_palindrome("racecar"))
+
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) > 0:
+        for arg in args:
+            is_pal = is_palindrome(arg)
+            result = 'PASS' if is_pal else 'FAIL'
+            is_str = 'is' if is_pal else 'is not'
+            print('{}: {} {} a palindrome'.format(result, repr(arg), is_str))
+    else:
+        print('Usage: {} string1 string2 ... stringN'.format(sys.argv[0]))
+        print('  checks if each argument given is a palindrome')
+
+
+if __name__ == '__main__':
+    main()
